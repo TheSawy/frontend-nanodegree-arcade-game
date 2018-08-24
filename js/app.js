c3247bbcea4e3dd4526bfffd51fc263e2417865a
Ahmed <AhmedElsawyrev@gmail.com>
2018-08-24 15:39:28 +0200

    feat: functioning Enemy's moves

--- js/app.js
+++ js/app.js
@@ -16,6 +16,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    this.x += this.speed * dt;
+    if(this.x > 506){
+        this.x -= 506;
+        this.speed = 100 + Math.floor(Math.random() * 222);
+    }
 };
 
 // Draw the enemy on the screen, required method for game
